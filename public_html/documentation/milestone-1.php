f9d54b3dc7c95a5314faeadd2757fc23ea415163
Erik Clevenger <eclevenger@cnm.edu>
2018-04-16 15:31:46 -0600

    add index and stylesheet to milestone-1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<section class="sectionFirst">
+			<div class="purpose splitThird">
+				<h2>Purpose</h2>
+				<p>This PWP should attract freelance clients.</p>
+			</div>
+			<div class="audience splitThird">
+				<h2>Audience</h2>
+				<p>Effectively this site is designed for those who could be considered both peers and clients.</p>
+			</div>
+			<div class="goal splitThird">
+				<h2>Goal</h2>
+				<p>To generate freelance interaction design and product strategy work.</p>
+			</div>
+		</section>
+		<section class="sectionSecond">
+			<div class="splitHalf">
+				<h2 class="personaName">Sierra Cook</h2>
+				<ul>
+					<li>30</li>
+					<li>Project Manager</li>
+					<li>Technology
+						<ul>
+							<li>MacBook Pro - OSX High Sierra 10.13.x</li>
+							<li>iPhone 8s - iOS 11.2.x</li>
+						</ul>
+					</li>
+					<li>Very limited on time. Works primarily with people more than technology.</li>
+				</ul>
+			</div>
+			<div class="splitHalf">
+				<ul>
+					<li><h4>Frustrations</h4>
+						<p>Finding designers who cannot code and coders who are not designers.</p>
+					</li>
+					<li><h4>Additional Info</h4>
+						<p>Is not the final decision maker at her company but does have some pull in managerial terms.</p>
+					</li>
+				</ul>
+			</div>
+		</section>
+	</body>
+</html>
