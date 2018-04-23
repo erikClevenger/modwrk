c584b07946641ea4dec1b4a253cace49e0f69835
Erik Clevenger <eclevenger@cnm.edu>
2018-04-22 19:51:33 -0600

    add milestone 2 docs

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2</title>
+	</head>
+	<body>
+	<section>
+		<h2>Content Strategy</h2>
+		<p>My site will employ a single page layout split in to 3 sections.</p>
+	</section>
+	<section>
+		<h2>Welcome &amp; About</h2>
+		<p>The first section of this site will act as a brief welcome and about me. In particular this is where I
+			would like to have a button to display my resume and another to download it. As the intention of this
+			website is to essentially attract potential work I want necessary information readily accessible. </p>
+	</section>
+	<section>
+		<h2>Portfolio</h2>
+		<p>The next section would be my portfolio examples. This is the section where I show what I believe is my
+			stronger work. </p>
+	</section>
+		<h2>Contact</h2>
+		<p>The final section will feature the required contact form.</p>
+	</body>
+</html>
