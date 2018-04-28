0416db536c0a297dc91fffb58aa899074a9dd69e
Erik Clevenger <eclevenger@cnm.edu>
2018-04-28 12:34:35 -0600

    added another section and a little more text on design goals

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,15 +12,27 @@
 	<section>
 		<h2>Welcome &amp; About</h2>
 		<p>The first section of this site will act as a brief welcome and about me. In particular this is where I
-			would like to have a button to display my resume and another to download it. As the intention of this
+			would like to have a button view my resume and c.v. As the intention of this
 			website is to essentially attract potential work I want necessary information readily accessible. </p>
+		<p>As far as the overall design goals go the focus of this site will be to make sure that it is very
+			clean with next to no "buttons" visible while also making it very clear that there are other places to
+			navigate to. I'm shooting for a mostly horizontal feeling layout.</p>
 	</section>
 	<section>
 		<h2>Portfolio</h2>
 		<p>The next section would be my portfolio examples. This is the section where I show what I believe is my
 			stronger work. </p>
+		<div>
+			<h3>Current Projects</h3>
+			<p>As my design and creative interests continue to merge together I want to include a sub-section of my
+				portfolio where I keep information about my current projects. This is where I want to include a more
+				ongoing and frequently updated portion of my site as opposed to the more static portions of the website.</p>
+			<p>Going forward this is where I plan on having GitHub and headless wordpress integrations live.</p>
+		</div>
 	</section>
+	<section>
 		<h2>Contact</h2>
 		<p>The final section will feature the required contact form.</p>
+	</section>
 	</body>
 </html>
