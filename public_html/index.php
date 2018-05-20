c96dc83ab87ec6265c2d5c59114652cb4ddd33a5
Erik Clevenger <eclevenger@cnm.edu>
2018-05-20 16:36:08 -0600

    when in doubt commit and push

--- public_html/index.php
+++ public_html/index.php
@@ -9,35 +9,89 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
-		<!-- Fullpage.js -->
+		<!-- Fullpage.js
 		<link rel="stylesheet" type="text/css" href="jquery.fullPage.css" />
 		<script src="vendors/jquery.easings.min.js"></script>
 		<script type="text/javascript" src="vendors/scrolloverflow.min.js"></script>
 		<script type="text/javascript" src="jquery.fullPage.js"></script>
+		-->
 
 
 		<title>Mod~Wrk</title>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-sm navbar-dark fixed-bottom mb-2">
-			<button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-					  aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"><img id="mwBoltMenu" src="./imgs/mwLogo.svg" alt="Open menu
-				button"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-				<div class="navbar-nav d-flex flex-row row-h1 justify-content-around">
-						<a href="#slideHome">Home</a>
-						<a href="#slideContact">Contact</a>
-						<a href="#slidePortfolio">Portfolio</a>
+		<div class="container">
+			<nav class="navbar navbar-expand-sm navbar-dark fixed-bottom pb-2">
+				<div class="collapse navbar-collapse" id="navbarContent">
+					<div class="navbar-nav d-flex flex-row row-h1 justify-content-center">
+						<a class="slideBtn" href="#slideContact">Contact</a>
+						<a class="slideBtn" href="#slideHome">Home</a>
+						<a class="slideBtn" href="#slidePortfolio">Portfolio</a>
+					</div>
 				</div>
+			</nav>
+		</div>
+		<div id="mainContent" class="">
+			<div id="scrollable" class="">
+				<section class="slide" id="slideContact">
+					<div class="container">
+						<div class="row mt-5 justify-content-center">
+							<h2 class="col-12 align-self-center text-center">Contact</h2>
+							<p class="col-12 col-lg-6 offset-lg-1 mb-4">Interested in working with me?<br>Found a bug?<br>Don't like my
+								face?<br><strong>Let me know.</strong></p>
+							<form class="col-12 justify-content-center" id="contactForm">
+								<div class="form-group col-lg-6 offset-lg-3">
+									<input type="text" class="form-control" id="nameInput" placeholder="Name">
+								</div>
+								<div class="form-group col-lg-6 offset-lg-3">
+									<input type="email" class="form-control" id="emailInput" placeholder="Email">
+								</div>
+								<div class="form-group col-lg-6 offset-lg-3">
+									<textarea class="form-control" id="messageTextArea" rows="6"
+												 placeholder="Message"></textarea>
+								</div>
+							</form>
+							<button class="btn btn-outline-secondary col-6 col-lg-3 mt-4">Submit</button>
+						</div>
+					</div>
+				</section>
+				<section class="slide" id="slideHome"></section>
+				<section class="slide" id="slidePortfolio">
+					<div class="container">
+						<div class="row mt-5 justify-content-center">
+							<h2 class="col-12 text-center">Portfolio</h2>
+							<p class="mb-4"> A small smattering of past work.</p>
+							<div id="verticalScroll" class="col-10 offset-lg-3 card-columns">
+								<div class="card bg-dark text-light">
+									<img class="card-img" src="https://picsum.photos/200?random" alt="placeholder images from
+									Picsum">
+									<div class="card-img-overlay">
+										<h4 id="portfolio-1" class=" portfolio-item portfolio">Portfolio Item 1</h4>
+									</div>
+								</div>
+								<div class="card bg-dark text-light">
+									<img class="card-img" src="https://picsum.photos/200?random" alt="placeholder images from Picsum">
+									<div class="card-img-overlay">
+										<h4 id="portfolio-2" class=" portfolio-item portfolio">Portfolio Item 2</h4>
+									</div>
+								</div>
+								<div class="card bg-dark text-light">
+									<img class="card-img" src="https://picsum.photos/200?random" alt="placeholder images from Picsum">
+									<div class="card-img-overlay">
+										<h4 id="portfolio-3" class=" portfolio-item portfolio">Portfolio Item 3</h4>
+									</div>
+								</div>
+								<div class="card bg-dark text-light">
+									<img class="card-img" src="https://picsum.photos/200?random" alt="placeholder images from Picsum">
+									<div class="card-img-overlay">
+										<h4 id="portfolio-4" class=" portfolio-item portfolio">Portfolio Item 4</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
-		</nav>
-		<section id="slideHome">
-		</section>
-		<section id="slideContact">
-		</section>
-		<section id="slidePortfolio">
-		</section>
+		</div>
 </body>
 </html>
