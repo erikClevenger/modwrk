72beba3b2c89aa9ebc3867bd434ca85efdc92671
Erik Clevenger <eclevenger@cnm.edu>
2018-06-03 10:58:59 -0600

    swift mailer install

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -2,9 +2,26 @@ $(document).ready(function() {
 	$("#dragend").dragend({
 		jumpToPage: 2
 		});
+
+	$(window).on("scroll", function() {
+		// logic here...
+		let wScroll = $(this).scrollLeft();
+
+		$('#paraImg').css({
+			'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+		});
+	});
 });
 
+//$(window).scroll(function() {
 
+//	let wScroll = $(this).scrollLeft();
+
+//	$('#paraImg').css({
+//		'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+//	});
+
+//});
 
 
 
