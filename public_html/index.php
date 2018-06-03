4a044baf9edc90fd5467843f05218f9c0bfa6ac5
Erik Clevenger <eclevenger@cnm.edu>
2018-06-03 15:00:34 -0600

    add modal

--- public_html/index.php
+++ public_html/index.php
@@ -34,6 +34,10 @@
 		<script src="js/dragend.min.js"></script>
 		<script src="js/scripts.js"></script>
 
+		<!-- jQuery Modal -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+
 
 		<title>Mod~Wrk</title>
 	</head>
