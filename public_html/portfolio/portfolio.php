00652d30609b668920d1a030ebed8bc98eab0ae7
Erik Clevenger <eclevenger@cnm.edu>
2018-06-20 15:54:18 -0600

    entire day of screwing around with hosting. added a bunch of portfolio content

--- public_html/portfolio/portfolio.php
+++ public_html/portfolio/portfolio.php
@@ -14,19 +14,64 @@ require_once(dirname(__DIR__) . "/php/head-utils.php");
 	</div>
 	<div class="animsition" data-animsition-in-class="fade-in-down-sm" data-animsition-out-class="fade-out-left-sm">
 		<section class="page " id="portfolio">
-		<div class="container">
-			<div id ="portfolioHeadline" class="row mt-5 justify-content-center">
-				<h2 class="col-12 text-center">Portfolio</h2>
-				<p class="mb-4"> A small smattering of past work.</p>
-			</div>
-			<div id="verticalScroll" class="row justify-content-center">
-				<div id="killerspin" class="col-10 portfolioItem data-fancybox ">
-
+			<div class="container">
+				<div id ="portfolioHeadline" class="row mt-5 justify-content-center">
+					<h2 class="col-12 text-center">Portfolio</h2>
+					<p class="mb-4"> A small smattering of past work.</p>
 				</div>
-				<div id="demarcated" class="col-10 portfolioItem"></div>
-				<div id="mypingo" class="col-10 portfolioItem"></div>
 			</div>
-		</div>
-	</section>
+			<div id="verticalScroll" class="container">
+				<a href="#" data-trigger="ks-gallery" id="killerspin" class="portfolioItem row"><img
+						class="featureImg" src="../imgs/ks/ks_FtrIMG.jpg" alt="Killerspin featured
+				image">
+				</a>
+				<div class="hide">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_Achieve.jpg" alt="Killerspin
+					achievements
+					mock up">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_Challenge.jpg"
+						  alt="Killerspin
+					challenge flow
+					mock up">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_Friends.jpg"
+						  alt="Killerspin friends view">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_Home.jpg" alt="Killerspin
+					friends view">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_Paddles.jpg" alt="Killerspin
+					equipment picker">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_Settings.jpg" alt="Killerspin
+					settings pane">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_Tables.jpg" alt="Killerspin tables
+					picker">
+					<img class="ks-gallery" data-fancybox="ks-gallery" src="../imgs/ks/sl_View.jpg" alt="Killerspin view
+					profile flow">
+				</div>
+				<a href="#" data-trigger="dm-gallery" id="demarcated" class="portfolioItem row"><img class="featureImg" src="../imgs/dm/book_out_tsp.png"></a>
+				<div class="hide">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-1.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-2.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-3.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-4.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-8.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-9.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-10.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/book-11.jpg" alt="book page">
+					<img class="col-10" data-fancybox="dm-gallery" src="../imgs/dm/cover_rect.jpg" alt="book page">
+				</div>
+				<a href="#" data-trigger="mp-gallery" id="mypingo" class="portfolioItem row"><img
+						class="featureImg" src="../imgs/mp/featured-image.jpg"></a>
+				<div class="hide">
+					<img class="mp-gallery" data-fancybox="mp-gallery" src="../imgs/mp/mp_BuyDevice.gif" alt="myPingo
+					purchase
+					flow">
+					<img class="mp-gallery" data-fancybox="mp-gallery" src="../imgs/mp/mp_Context.gif" alt="myPingo tool
+					menu
+					 interaction flow">
+					<img class="mp-gallery" data-fancybox="mp-gallery" src="../imgs/mp/mp_LikeDevice.gif" alt="myPingo image
+					like interaction flow">
+				</div>
+			</div>
+
+		</section>
 	</div>
 </body>
